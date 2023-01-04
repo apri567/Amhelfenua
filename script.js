9bd3146841dd7b07f4f7180e38fbdbaf6da78221
apri <apric567@gmail.com>
2023-01-05 00:58:06 +0200

    Added a "Wishes" button to the "Help" section. Added a section with reviews

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function showMore (e) {
         rowsList[i].classList.toggle('d-block');
     }
 }
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
