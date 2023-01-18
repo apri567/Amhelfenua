df141af528e4447f79b0b60bfcefbaa51fea1ed4
apri <apric567@gmail.com>
2023-01-18 20:07:50 +0200

    Made edit in previous sections. Changed background. Added contacts sections and politics page

--- script.js
+++ script.js
@@ -33,3 +33,10 @@ const swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+const industryFormBtn = document.querySelector('#industryFormBtn');
+
+industryFormBtn.addEventListener('click', () => {
+    window.location.href = 'industry-form.html';
+})
